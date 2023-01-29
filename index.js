287effa94e39a784465cfc785b9c8281c516c64c
Francisco Júnior <franciscojr.dev@gmail.com>
2023-01-29 16:43:14 -0300

    Changes

--- index.js
+++ index.js
@@ -2,15 +2,25 @@ const WebSocket = require('ws');
 const server = new WebSocket.Server({
     port: process.env.PORT || 8080
 });
+const balanceDefault = process.env.BALANCE_DEFAULT || 100;
 let sockets = [];
+let pixelStorage = [];
 
 console.log(`Started :${server.options.port}`);
 
 server.on('connection', (socket) => {
-    sockets.push(socket);
+    sockets.push({socket, balance: balanceDefault});
 
     console.log('New client connected!');
-    sendData(socket, {status: "ok"});
+
+    sendData(
+        socket,
+        {
+            type: "load",
+            balance: balanceDefault,
+            data: pixelStorage
+        }
+    );
     
     socket.on('message', (data) => {
         try {
@@ -19,15 +29,25 @@ server.on('connection', (socket) => {
             if (content.type !== 'register') {
                 return;
             }
+
+            pixelStorage.push(content.data);
     
-            sockets.forEach(s => sendData(s, content));
+            sockets.forEach(
+                s => {
+                    if (s.socket === socket) {
+                        s.balance -= s.balance > 0 ? 1 : 0;
+                    }
+
+                    sendData(s.socket, {...content, balance: s.balance});
+                }
+            );
         } catch (e) {
             console.log('Not allowed message format!');
         }
     });
     
     socket.on('close', () => {
-        sockets = sockets.filter(s => s !== socket);
+        sockets = sockets.filter(s => s.socket !== socket);
     });
 });
 
